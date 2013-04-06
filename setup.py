7a51d118e28a9879c406680923a22b8f6ad8e4ad
Yuval Pavel Zholkover <paulzhol@gmail.com>
2013-04-06 17:38:41 +0300

    bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "libtask9",
-    version = 0.2,
+    version = 0.2.1,
     description = 'Python port of the Plan 9 from User Space (aka plan9port) version of libthread',
     author = 'Yuval Pavel Zholkover',
     author_email = 'paulzhol@gmail.com',
